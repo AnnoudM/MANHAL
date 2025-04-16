9d9b57481b0956efa262c8acb3ec5448d57775a2
Raghad-Alim <raghad.aliali44@gmail.com>
2025-04-16 18:24:49 +0300

    Add word category thresholds and mappings for stickers logic

---
+++ lib/constants/word_categories.dart
@@ -0,0 +1,55 @@
+const Map<String, int> wordCategoryThreshold = {
+  'shapes': 8,
+  'colors': 10,
+  'animals': 10,
+  'food': 11,
+};
+
+const Map<String, String> wordToCategory = {
+  // 🟣 Shapes
+  'دائرة': 'shapes',
+  'قلب': 'shapes',
+  'مثلث': 'shapes',
+  'مربع': 'shapes',
+  'مستطيل': 'shapes',
+  'معين': 'shapes',
+  'نجمة': 'shapes',
+  'هلال': 'shapes',
+
+  // 🟡 Colors
+  'أبيض': 'colors',
+  'أحمر': 'colors',
+  'أخضر': 'colors',
+  'أزرق': 'colors',
+  'أسود': 'colors',
+  'أصفر': 'colors',
+  'بنفسجي': 'colors',
+  'بني': 'colors',
+  'رمادي': 'colors',
+  'وردي': 'colors',
+
+  // 🐾 Animals
+  'أسد': 'animals',
+  'بقرة': 'animals',
+  'دجاجة': 'animals',
+  'زرافة': 'animals',
+  'سنجاب': 'animals',
+  'خفاش': 'animals',
+  'سلحفاة': 'animals',
+  'غراب': 'animals',
+  'فهد': 'animals',
+  'قندس': 'animals',
+
+  // 🍏 Food
+  'عنب': 'food',
+  'جزر': 'food',
+  'خوخ': 'food',
+  'بطاطس': 'food',
+  'برتقالة': 'food',
+  'حليب': 'food',
+  'خبز': 'food',
+  'خيار': 'food',
+  'فراولة': 'food',
+  'موزة': 'food',
+  'رمان': 'food',
+};
