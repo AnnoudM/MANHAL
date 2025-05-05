48c0316cfa561efe8540a7ca801339b684694bce
Raghad <RaghadKhalid.sa@outlook.com>
2025-05-05 15:22:00 +0300

    update

--- integration_test/create_account_test.dart
+++ integration_test/create_account_test.dart
@@ -2,74 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:manhal/main.dart' as app;
-import 'package:firebase_auth/firebase_auth.dart';
-
-
-Future<void> login(WidgetTester tester) async {
-  app.main();
-  await tester.pumpAndSettle(const Duration(seconds: 10));
-
-  final loginButton = find.text('تسجيل الدخول');
-  expect(loginButton, findsOneWidget);
-  await tester.tap(loginButton);
-  await tester.pumpAndSettle();
-
-  await tester.enterText(
-      find.byType(TextFormField).at(0), 'Raghadkhalid.sa@outlook.com');
-  await tester.enterText(find.byType(TextFormField).at(1), 'A12345\$');
-
-  final continueButton = find.text('تسجيل');
-  expect(continueButton, findsOneWidget);
-  await tester.tap(continueButton);
-  await tester.pumpAndSettle(const Duration(seconds: 5));
-}
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Add child with valid data ', (tester) async {
-    await login(tester);
+  testWidgets('Create Account - valid information', (tester) async {
+    app.main();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
+    await tester.tap(find.text('الانضمام إلى منهل'));
+    await tester.pumpAndSettle(const Duration(seconds: 3));
 
-    final addChildButton = find.text('طفل جديد');
-    expect(addChildButton, findsOneWidget);
-    await tester.tap(addChildButton);
-    await tester.pumpAndSettle();
-
-    final editIcon = find.byIcon(Icons.edit);
-    expect(editIcon, findsOneWidget);
-    await tester.tap(editIcon);
-    await tester.pumpAndSettle();
-
-    final avatarOption = find.byType(GestureDetector).at(10);
-    await tester.tap(avatarOption);
+    final fields = find.byType(TextFormField);
+    await tester.enterText(fields.at(0), 'نور'); 
+    await tester.enterText(fields.at(1), 'parent@example.com'); 
+    await tester.enterText(fields.at(2), 'Password123.'); 
+    await tester.enterText(fields.at(3), 'Password123.'); 
     await tester.pumpAndSettle();
 
-    final saveButton = find.text('حفظ');
-    expect(saveButton, findsOneWidget);
-    await tester.tap(saveButton);
-    await tester.pumpAndSettle();
-
-    await tester.enterText(find.byType(TextFormField).at(0), 'سلمان');
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.byType(DropdownButtonFormField<String>).at(0));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('ذكر').first);
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.byType(DropdownButtonFormField<String>).at(1));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('٤').first);
-    await tester.pumpAndSettle();
-
-    final confirmButton = find.text('إضافة');
-    expect(confirmButton, findsOneWidget);
-    await tester.tap(confirmButton);
+    await tester.tap(find.text('متابعة'));
     await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(find.text('سجل طفلك الأول'), findsOneWidget);
+    await tester.enterText(fields.at(0), 'سلمان'); 
+    await tester.tap(find.byType(DropdownButton<String>).at(0)); 
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('ذكر').last); 
+    await tester.pumpAndSettle();
+    await tester.tap(find.byType(DropdownButtonFormField<String>).at(1)); 
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('٤').first); 
+    await tester.pumpAndSettle();
 
-    expect(find.textContaining('تمت إضافة الطفل بنجاح'), findsOneWidget);
-    await FirebaseAuth.instance.signOut();
-
-    print('Add child with photo test passed!');
+    await tester.tap(find.text('تسجيل'));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    expect(find.text('تم إرسال رابط التحقق'), findsOneWidget);
+    
+    await tester.tap(find.text('حسناً'));
+    await tester.pumpAndSettle();
+    print('Test passed! '); 
   });
 }
--- integration_test/view_ethical_value_test.dart
+++ integration_test/view_ethical_value_test.dart
@@ -5,22 +5,18 @@ import 'package:manhal/main.dart' as app;
 
 Future<void> login(WidgetTester tester) async {
   app.main();
-  // انتظر حتى استقرار الصفحة بالكامل
   await tester.pumpAndSettle(const Duration(seconds: 10));
 
-  // تحقق من وجود زر تسجيل الدخول
   final loginButton = find.text('تسجيل الدخول');
   expect(loginButton, findsOneWidget);
   await tester.tap(loginButton);
   await tester.pumpAndSettle();
 
-  // إدخال بيانات الاعتماد
   print('إدخال بيانات الاعتماد...');
   await tester.enterText(
       find.byType(TextFormField).at(0), 'Raghadkhalid.sa@outlook.com');
   await tester.enterText(find.byType(TextFormField).at(1), 'A12345\$');
 
-  // تحقق من وجود زر "تسجيل"
   final continueButton = find.text('تسجيل');
   expect(continueButton, findsOneWidget);
   await tester.tap(continueButton);
@@ -32,37 +28,28 @@ Future<void> login(WidgetTester tester) async {
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('التحقق من شاشة القيم الأخلاقية', (tester) async {
-    // تسجيل الدخول أولًا
-    await login(tester);
+  testWidgets('التحقق من الضغط على القيمة الأخلاقية "الصدق"', (tester) async {
+  await login(tester);
 
-    // التحقق من وجود اسم الطفل "غنى"
-    final raghadChild = find.text('غنى').first;
-    expect(raghadChild, findsOneWidget);
-    await tester.tap(raghadChild);
-    await tester.pumpAndSettle();
+  final raghadChild = find.text('لولو').first;
+  expect(raghadChild, findsOneWidget);
+  await tester.tap(raghadChild);
+  await tester.pumpAndSettle();
 
-    print('التحقق من وجود زر القيم الأخلاقية...');
-    final ethicsButton = find.text('القيم الأخلاقية');
-    expect(ethicsButton, findsOneWidget);
-    await tester.tap(ethicsButton);
-    await tester.pumpAndSettle(const Duration(seconds: 5));  
+  final ethicsButton = find.text('القيم الأخلاقية');
+  expect(ethicsButton, findsOneWidget);
+  await tester.tap(ethicsButton);
+  await tester.pumpAndSettle(const Duration(seconds: 5));  
 
-    // الانتظار لفترة إضافية
-    await tester.pumpAndSettle(const Duration(seconds: 15)); 
+  await tester.pumpAndSettle(const Duration(seconds: 15)); 
 
-    // التحقق من وجود القيم الأخلاقية على المسار
-    final valuesOnPath = [
-      'التعاون', 'الإحسان', 'الأمانة', 'الصدق'
-    ];
+  final valueWidget = find.widgetWithText(Container, 'الصدق');
+  expect(valueWidget, findsOneWidget); 
 
-    // البحث عن كل قيمة أخلاقية على المسار
-    for (var value in valuesOnPath) {
-      final valueWidget = find.widgetWithText(Container, value);
-      expect(valueWidget, findsOneWidget);
-      print('✅ تم العثور على القيمة الأخلاقية "$value"!');
-    }
+  await tester.tap(valueWidget);  
+  await tester.pumpAndSettle(); 
 
-    print('✅ جميع القيم الأخلاقية تم عرضها بنجاح!');
-  });
+  expect(find.text('الصدق'), findsOneWidget);
+});
+
 }
